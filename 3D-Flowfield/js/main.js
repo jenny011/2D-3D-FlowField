3707e27fcfb2d146871c17893f5e4c49c652fd5b
Jenny Zhu <jz2915@nyu.edu>
2019-05-16 00:27:23 +0800

    05/15/2019-load music

--- 3D-Flowfield/js/main.js
+++ 3D-Flowfield/js/main.js
@@ -55,7 +55,7 @@ gui.add(params, 'zLength', 7, 15,1);
 
 	// load a sound and set it as the Audio object's buffer
 	var audioLoader = new THREE.AudioLoader();
-	audioLoader.load( 'audio/話がしたいよ.mp3', function( buffer ) {
+	audioLoader.load( 'audio/BumpOfChicken.mp3', function( buffer ) {
 		//Claude Debussy - La fille aux cheveux de lin
 		sound.setBuffer( buffer );
 		sound.setLoop(true);
